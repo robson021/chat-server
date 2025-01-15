b4499700e670c27f8af5b756af8adcb803d03928
Robert <nowak021@gmail.com>
2025-01-15 22:34:51 +0100

    log only console

--- src/logger_config.rs
+++ src/logger_config.rs
@@ -8,33 +8,42 @@ use log4rs::encode::pattern::PatternEncoder;
 const LOG_PATTERN: &str = "{d} |{l}|: {m}{n}";
 
 pub fn setup_logger() {
-    if cfg!(debug_assertions) {
-        let stderr = ConsoleAppender::builder()
-            .encoder(Box::new(PatternEncoder::new(LOG_PATTERN)))
-            .target(Target::Stderr)
-            .build();
+    debug_config();
+    // if cfg!(debug_assertions) {
+    //     debug_config();
+    // } else {
+    //     prod_config();
+    // }
+}
 
-        let error_trace_level = Root::builder().appender("stderr").build(LevelFilter::Trace);
+fn prod_config() {
+    let logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::default()))
+        .build("chat-server.log")
+        .unwrap();
 
-        let console_cfg = Config::builder()
-            .appender(Appender::builder().build("stderr", Box::new(stderr)))
-            .build(error_trace_level)
-            .unwrap();
+    let file_cfg = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder().appender("logfile").build(LevelFilter::Info))
+        .unwrap();
 
-        log4rs::init_config(console_cfg).unwrap();
-        log::set_max_level(LevelFilter::Debug);
-    } else {
-        let logfile = FileAppender::builder()
-            .encoder(Box::new(PatternEncoder::default()))
-            .build("chat-server.log")
-            .unwrap();
+    log4rs::init_config(file_cfg).unwrap();
+    log::set_max_level(LevelFilter::Info);
+}
 
-        let file_cfg = Config::builder()
-            .appender(Appender::builder().build("logfile", Box::new(logfile)))
-            .build(Root::builder().appender("logfile").build(LevelFilter::Info))
-            .unwrap();
+fn debug_config() {
+    let stderr = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(LOG_PATTERN)))
+        .target(Target::Stderr)
+        .build();
 
-        log4rs::init_config(file_cfg).unwrap();
-        log::set_max_level(LevelFilter::Info);
-    }
+    let error_trace_level = Root::builder().appender("stderr").build(LevelFilter::Trace);
+
+    let console_cfg = Config::builder()
+        .appender(Appender::builder().build("stderr", Box::new(stderr)))
+        .build(error_trace_level)
+        .unwrap();
+
+    log4rs::init_config(console_cfg).unwrap();
+    log::set_max_level(LevelFilter::Debug);
 }
