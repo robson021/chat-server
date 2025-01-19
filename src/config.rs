c862388c5801106199bad4e8666df3f3afdd100e
Robert <nowak021@gmail.com>
2025-01-19 15:29:12 +0100

    test loading history from file

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,3 @@
-use log::LevelFilter;
-use log4rs::append::console::{ConsoleAppender, Target};
-use log4rs::config::{Appender, Root};
-use log4rs::encode::pattern::PatternEncoder;
-
 const LOG_FILE_PATH: &str = "./resources/logger_cfg.yml";
 const LOG_PATTERN: &str = "{h({d(%Y-%m-%d %H:%M:%S)(utc)} - {l}: {m}{n})}";
 
@@ -23,49 +18,16 @@ impl Config {
     pub fn setup_logger(&self) {
         match self.profile {
             Profile::Release => log4rs::init_file(LOG_FILE_PATH, Default::default()).unwrap(),
-            Profile::Dev => {
-                let stderr = ConsoleAppender::builder()
-                    .encoder(Box::new(PatternEncoder::new(LOG_PATTERN)))
-                    .target(Target::Stderr)
-                    .build();
-
-                let error_trace_level =
-                    Root::builder().appender("stderr").build(LevelFilter::Trace);
-
-                let console_cfg = log4rs::Config::builder()
-                    .appender(Appender::builder().build("stderr", Box::new(stderr)))
-                    .build(error_trace_level)
-                    .unwrap();
-
-                log4rs::init_config(console_cfg).unwrap();
-                log::set_max_level(LevelFilter::Debug);
-            }
+            Profile::Dev => log4rs::init_file(LOG_FILE_PATH, Default::default()).unwrap(),
         };
     }
 }
 
 pub fn get_config() -> Config {
-    if cfg!(debug_assertions) {
-        Config {
-            profile: Profile::Dev,
-            password: None,
-            log_file: None,
-            host: "localhost:8080".to_owned(),
-        }
-    } else {
-        let args: Vec<String> = std::env::args().collect();
-        if args.len() < 2 {
-            panic!("No password specified");
-        }
-        let password = args[1].trim().to_owned();
-        if password.len() < 5 {
-            panic!("Password must be at least 5 characters");
-        }
-        Config {
-            profile: Profile::Release,
-            password: Some(password),
-            host: "0.0.0.0:8080".to_owned(),
-            log_file: Some("./log/chat-server.log".to_owned()),
-        }
+    Config {
+        profile: Profile::Dev,
+        password: None,
+        host: "localhost:8081".to_owned(),
+        log_file: Some("./log/chat-server.log".to_owned()),
     }
 }
